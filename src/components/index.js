62dd6e89d34120291f3cb7d96a5abecc50a62630
Bahriddin-tiger <934045436b@gmail.com>
2022-08-21 12:08:50 +0500

    Initiall commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
 import {List} from "./List";
 import {Item} from "./Item";
-import {Select} from "./Select";
 import { ApexChart } from "./Apexchart";
 import { Header } from "./Header";
 import { Static } from "./Static";
 import { Costumer } from "./Costumer";
-export  {List,Item,Select, ApexChart,Header,Static, Costumer} ;
+export  {List,Item, ApexChart,Header,Static, Costumer} ;
